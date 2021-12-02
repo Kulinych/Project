9a0eb1911917e453cd21a1cd48704c6622abbb29
Sergei Kulina <kulinych@tut.by>
2021-12-02 21:34:40 +0300

    fix path

--- todolist/settings.py
+++ todolist/settings.py
@@ -61,22 +61,13 @@ WSGI_APPLICATION = "todolist.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-#     }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'todolist',
-        'USER': 'test',
-        'PASSWORD': 'test',
-        'HOST': 'md-sa2-18-21-mariadb',
-        'PORT': '3306'
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "/var/sqlite/db.sqlite3"),
     }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
